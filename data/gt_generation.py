9df838afc06345a6c372b5b6df6f30c50b8e3ed1
andrewzl <andrewzouli@gmail.com>
2023-03-17 17:05:56 -0400

    update ground truth generation

--- data/gt_generation.py
+++ data/gt_generation.py
@@ -24,3 +24,30 @@ def clean_dir(output_dir):
     for file in os.listdir(output_dir):
         shutil.rmtree(output_dir + file)
 
+
+def generate_long_exposure():
+    thresh = 10
+
+    os.mkdir("/kaggle/working/ground_truth")
+    root = '/kaggle/input/aps360-sky-train-dataset/'
+    exist = os.listdir('/kaggle/working/ground_truth/')
+
+    for folder in os.listdir(root):
+        for subfolder in os.listdir(root + folder):
+            savename = subfolder + '.jpg'
+            if savename in exist:
+                continue
+            filenames = os.listdir(root + folder + '/' + subfolder)
+            prefix = root + folder + '/' + subfolder + '/'
+            photo_seq = np.array([np.array(Image.open(prefix + fname)) for fname in filenames])
+
+            mean_frame = np.array(np.mean(photo_seq, axis=0), dtype=np.uint8)
+            long_exposure = Image.fromarray(mean_frame)
+
+            # only save if laplace variance is greater than the threshold
+            gray_image = cv2.cvtColor(mean_frame, cv2.COLOR_RGB2GRAY)
+            laplace_var = cv2.Laplacian(gray_image, cv2.CV_64F).var()
+
+            if laplace_var > thresh:
+                long_exposure.save(f"/kaggle/working/ground_truth/{subfolder}.jpg")
+
